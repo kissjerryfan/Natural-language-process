f005ce3e7506c5954c941c8b6d0ce540d17399e8
kissjerryfan <jerryfan@connect.hku.hk>
2024-12-21 01:06:03 +0800

    commit order and delete libaray tool

---
+++ order.py
@@ -0,0 +1,39 @@
+def order():
+    """
+        order food in Xiangjiang Restaurant.
+
+        Args:
+        None
+
+        Returns:
+        None
+        """
+    from selenium import webdriver
+    from selenium.webdriver.common.by import By
+    import time
+    driver = webdriver.Chrome()
+
+    driver.get("https://csd2.order.place/store/112841/mode/takeaway")
+    time.sleep(3)
+    start = driver.find_element(By.CLASS_NAME, 'start-btn-box')
+    start.click()
+    for handle in driver.window_handles:
+        driver.switch_to.window(handle)
+    time.sleep(3)
+    choose = driver.find_element(By.XPATH,
+                                 '//*[@id="content"]/category-list-page/ion-content/ion-grid/ion-row/ion-col[1]')
+    choose.click()
+
+    time.sleep(3)
+    add = driver.find_element(By.XPATH, '//*[@id="content"]/item-grid-scroll-page/ion-content/div/div/div[1]')
+    add.click()
+
+    time.sleep(3)
+    addToOrder = driver.find_element(By.XPATH, '//*[@id="footer-box"]/div[2]/div[2]')
+    addToOrder.click()
+    time.sleep(3)
+    bill = driver.find_element(By.XPATH, '//*[@id="content"]/item-grid-scroll-page/ion-footer/ion-button')
+    bill.click()
+    time.sleep(19)
+
+
--- quick_start.py
+++ quick_start.py
@@ -1,12 +1,15 @@
 from oscopilot import FridayAgent
 from oscopilot import ToolManager
 from oscopilot import FridayExecutor, FridayPlanner, FridayRetriever
+# from examples.light_friday import LightFriday
 from oscopilot.utils import setup_config, setup_pre_run
 
 args = setup_config()
 if not args.query:
-    args.query = "Copy any text file located in the working_dir/document directory that contains the word 'agent' to a new folder named 'agents' "
-    args.query = "create a profile.csv file in working_dir and create some fake profile data in it"
+    # args.query = "Copy any text file located in the working_dir/document directory that contains the word 'agent' to a new folder named 'agents' "
+    # args.query = "create a profile.csv file in working_dir and create some fake profile data in it"
+    args.query = "please help me order in Xiangjiang"
 task = setup_pre_run(args)
 agent = FridayAgent(FridayPlanner, FridayRetriever, FridayExecutor, ToolManager, config=args)
+# agent = LightFriday(config=args)
 agent.run(task=task)
